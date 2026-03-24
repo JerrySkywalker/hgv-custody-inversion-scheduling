1bff87bfaa676256ac2c77997c34f018c90076c3
JerrySkywalker <jerry1999_main@outlook.com>
2026-03-24 17:55:33 +0800

    feat: support feasible-only and sorted slice views

--- framework/managers/static/services/slice_service.m
+++ framework/managers/static/services/slice_service.m
@@ -13,7 +13,21 @@ if isfield(slice_spec, 'mode') && ~isempty(slice_spec.mode)
     slice_mode = upper(string(slice_spec.mode));
 end
 
+feasible_only = false;
+if isfield(slice_spec, 'feasible_only') && ~isempty(slice_spec.feasible_only)
+    feasible_only = logical(slice_spec.feasible_only);
+end
+
+sort_by = '';
+if isfield(slice_spec, 'sort_by') && ~isempty(slice_spec.sort_by)
+    sort_by = string(slice_spec.sort_by);
+end
+
 tbl = truth_result.table;
+
+if feasible_only
+    tbl = tbl(tbl.is_feasible, :);
+end
 
 switch char(slice_mode)
     case 'PT'
@@ -27,8 +41,22 @@ end
 
 slice_table = tbl(:, vars);
 
+switch char(sort_by)
+    case 'Ns_asc'
+        slice_table = sortrows(slice_table, {'Ns','joint_margin'}, {'ascend','descend'});
+    case 'joint_margin_desc'
+        slice_table = sortrows(slice_table, {'joint_margin','Ns'}, {'descend','ascend'});
+    case ''
+        % no sort
+    otherwise
+        error('slice_service:UnsupportedSort', ...
+            'Unsupported sort_by option: %s', sort_by);
+end
+
 slice_result = struct();
 slice_result.mode = char(slice_mode);
+slice_result.feasible_only = feasible_only;
+slice_result.sort_by = char(sort_by);
 slice_result.table = slice_table;
 slice_result.row_count = height(slice_table);
 slice_result.meta = struct('status', 'ok');
