ca65f4b791c1f35aecb262ade6f6c259a731dfa9
JerrySkywalker <jerry1999_main@outlook.com>
2026-03-24 17:55:41 +0800

    refactor: make MB PT slice use feasible-only sorted view

--- experiments/chapter4/milestones/MB/run_MB_PT_slice.m
+++ experiments/chapter4/milestones/MB/run_MB_PT_slice.m
@@ -4,7 +4,12 @@ startup;
 mgr = create_static_evaluation_manager(make_profile_MB_nominal());
 out = mgr.run();
 
-slice_result = slice_service(out.truth_result, struct('mode', 'PT'));
+slice_spec = struct();
+slice_spec.mode = 'PT';
+slice_spec.feasible_only = true;
+slice_spec.sort_by = 'Ns_asc';
+
+slice_result = slice_service(out.truth_result, slice_spec);
 
 disp('[experiment] MB PT slice completed.');
 disp(slice_result.table);
