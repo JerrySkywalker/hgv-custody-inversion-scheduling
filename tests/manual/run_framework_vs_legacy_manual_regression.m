43af2aa28f4420fe2436f3c73e1b9580f86e76ac
JerrySkywalker <jerry1999_main@outlook.com>
2026-03-25 16:40:00 +0800

    test: add manual framework-vs-legacy regression runner

---
+++ tests/manual/run_framework_vs_legacy_manual_regression.m
@@ -0,0 +1,13 @@
+function report = run_framework_vs_legacy_manual_regression()
+startup;
+
+report = struct();
+report.stage01 = manual_compare_stage01_casebank();
+report.stage02 = manual_compare_stage02_nominal_traj();
+report.stage03_resource = manual_compare_stage03_resource();
+report.stage03_visibility = manual_compare_stage03_visibility();
+report.stage04_window = manual_compare_stage04_window();
+
+disp('=== Framework vs Legacy Manual Regression ===');
+disp(report);
+end
