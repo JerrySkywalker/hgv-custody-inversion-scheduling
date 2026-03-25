46067cd4a4e4b82cb1e113c0368bda01c5f89acc
JerrySkywalker <jerry1999_main@outlook.com>
2026-03-25 16:54:00 +0800

    fix: use correct legacy stage01 and stage02 entrypoints in visibility regression

--- tests/manual/manual_compare_stage03_visibility.m
+++ tests/manual/manual_compare_stage03_visibility.m
@@ -1,21 +1,22 @@
 function out = manual_compare_stage03_visibility()
 cfg = default_params();
 
-legacy_casebank = stage01_build_casebank(cfg);
+legacy_casebank = build_casebank_stage01(cfg);
 case_item = legacy_casebank.nominal(1);
-legacy_traj = stage02_propagate_hgv_case(case_item, cfg);
+legacy_traj = propagate_hgv_case_stage02(case_item, cfg);
 
 traj_case = struct('case', case_item, 'traj', legacy_traj);
 
 legacy_walker = build_single_layer_walker_stage03(cfg);
 legacy_satbank = propagate_constellation_stage03(legacy_walker, legacy_traj.t_s);
 
-engine_walker = build_single_layer_walker(struct( ...
+design_row = struct( ...
     'h_km', cfg.stage03.h_km, ...
     'i_deg', cfg.stage03.i_deg, ...
     'P', cfg.stage03.P, ...
     'T', cfg.stage03.T, ...
-    'F', cfg.stage03.F), cfg);
+    'F', cfg.stage03.F);
+engine_walker = build_single_layer_walker(design_row, cfg);
 engine_satbank = propagate_constellation(engine_walker, legacy_traj.t_s, cfg);
 
 legacy_vis = compute_visibility_matrix_stage03(traj_case, legacy_satbank, cfg);
