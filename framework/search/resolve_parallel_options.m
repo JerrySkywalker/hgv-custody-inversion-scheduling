9045275b40fb72869a3509280d46d9e1e6c932c4
JerrySkywalker <jerry1999_main@outlook.com>
2026-03-26 00:13:32 +0800

    feat: add min row threshold for enabling parallel search

--- framework/search/resolve_parallel_options.m
+++ framework/search/resolve_parallel_options.m
@@ -8,6 +8,7 @@ opts.use_parallel = false;
 opts.pool_profile = 'local';
 opts.num_workers = [];
 opts.show_progress = true;
+opts.min_parallel_rows = 20;
 
 if isfield(search_spec, 'use_parallel')
     opts.use_parallel = logical(search_spec.use_parallel);
@@ -20,5 +21,8 @@ if isfield(search_spec, 'num_workers')
 end
 if isfield(search_spec, 'show_progress')
     opts.show_progress = logical(search_spec.show_progress);
+end
+if isfield(search_spec, 'min_parallel_rows') && ~isempty(search_spec.min_parallel_rows)
+    opts.min_parallel_rows = search_spec.min_parallel_rows;
 end
 end
