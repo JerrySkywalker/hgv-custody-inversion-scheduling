9890654bc9f4dcaa440208ee955e5bca5a01bc96
JerrySkywalker <jerry1999_main@outlook.com>
2026-03-26 00:13:14 +0800

    fix: remove parfor temporary variable warning in parallel search helper

--- framework/search/run_design_grid_search_parallel.m
+++ framework/search/run_design_grid_search_parallel.m
@@ -20,29 +20,16 @@ if parallel_opts.use_parallel
 
         switch engine_mode
             case 'opend'
-                eval_out = evaluate_design_point_opend(row, trajs_in, gamma_eff_scalar, engine_cfg);
+                packed = local_pack_parallel_row( ...
+                    row, evaluate_design_point_opend(row, trajs_in, gamma_eff_scalar, engine_cfg), ...
+                    engine_mode, gamma_eff_scalar, source_kind);
             case 'closedd'
-                eval_out = evaluate_design_point_closedd(row, trajs_in, gamma_eff_scalar, engine_cfg);
+                packed = local_pack_parallel_row( ...
+                    row, evaluate_design_point_closedd(row, trajs_in, gamma_eff_scalar, engine_cfg), ...
+                    engine_mode, gamma_eff_scalar, source_kind);
             otherwise
                 error('run_design_grid_search_parallel:UnsupportedMode', ...
                     'Unsupported evaluator mode: %s', engine_mode);
-        end
-
-        packed = row;
-        eval_fields = fieldnames(eval_out);
-        for i = 1:numel(eval_fields)
-            f = eval_fields{i};
-            packed.(f) = eval_out.(f);
-        end
-
-        if ~isfield(packed, 'gamma_eff_scalar')
-            packed.gamma_eff_scalar = gamma_eff_scalar;
-        end
-        if ~isfield(packed, 'engine_mode')
-            packed.engine_mode = string(engine_mode);
-        end
-        if ~isfield(packed, 'source_kind')
-            packed.source_kind = string(source_kind);
         end
 
         result_cells{k} = packed;
@@ -55,4 +42,23 @@ else
 end
 
 result_rows = vertcat(result_cells{:});
+end
+
+function packed = local_pack_parallel_row(row, eval_out, engine_mode, gamma_eff_scalar, source_kind)
+packed = row;
+eval_fields = fieldnames(eval_out);
+for i = 1:numel(eval_fields)
+    f = eval_fields{i};
+    packed.(f) = eval_out.(f);
+end
+
+if ~isfield(packed, 'gamma_eff_scalar')
+    packed.gamma_eff_scalar = gamma_eff_scalar;
+end
+if ~isfield(packed, 'engine_mode')
+    packed.engine_mode = string(engine_mode);
+end
+if ~isfield(packed, 'source_kind')
+    packed.source_kind = string(source_kind);
+end
 end
