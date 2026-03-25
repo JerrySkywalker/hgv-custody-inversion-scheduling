9297fc2e11c4497f5ec90e41a437e9bc2377ce9b
JerrySkywalker <jerry1999_main@outlook.com>
2026-03-25 17:08:42 +0800

    test: include stage05 OpenD small-set in manual regression runner

--- tests/manual/run_framework_vs_legacy_manual_regression.m
+++ tests/manual/run_framework_vs_legacy_manual_regression.m
@@ -8,6 +8,7 @@ report.stage03_resource = manual_compare_stage03_resource();
 report.stage03_visibility = manual_compare_stage03_visibility();
 report.stage04_window = manual_compare_stage04_window();
 report.stage06_heading_family = manual_compare_stage06_heading_family();
+report.stage05_opend_smallset = manual_compare_stage05_opend_smallset();
 
 disp('=== Framework vs Legacy Manual Regression ===');
 disp(report);
