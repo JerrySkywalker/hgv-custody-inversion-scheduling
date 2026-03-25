e714dce45a4571a04587a11cf3e0c6408ce2e819
JerrySkywalker <jerry1999_main@outlook.com>
2026-03-25 09:53:44 +0800

    feat: expose stage04 window length from nominal threshold helper

--- experiments/chapter4/validation/load_stage04_nominal_gamma_req.m
+++ experiments/chapter4/validation/load_stage04_nominal_gamma_req.m
@@ -17,11 +17,18 @@ S4 = load(stage04_file);
 assert(isfield(S4, 'out'), 'Invalid Stage04 cache: missing out field.');
 assert(isfield(S4.out, 'gamma_meta') && isstruct(S4.out.gamma_meta), ...
     'Invalid Stage04 cache: missing out.gamma_meta.');
+assert(isfield(S4.out, 'cfg') && isstruct(S4.out.cfg), ...
+    'Invalid Stage04 cache: missing out.cfg.');
+assert(isfield(S4.out.cfg, 'stage04') && isstruct(S4.out.cfg.stage04), ...
+    'Invalid Stage04 cache: missing out.cfg.stage04.');
 
 gamma_meta = S4.out.gamma_meta;
+stage04_cfg = S4.out.cfg.stage04;
 
 assert(isfield(gamma_meta, 'gamma_req'), ...
     'Invalid Stage04 gamma_meta: missing gamma_req.');
+assert(isfield(stage04_cfg, 'Tw_s'), ...
+    'Invalid Stage04 cfg.stage04: missing Tw_s.');
 
 info = struct();
 info.gamma_req = gamma_meta.gamma_req;
@@ -34,6 +41,8 @@ else
     info.gamma_source = 'stage04_nominal_quantile';
 end
 
+info.Tw_s = stage04_cfg.Tw_s;
 info.cache_file = stage04_file;
 info.gamma_meta = gamma_meta;
+info.stage04_cfg = stage04_cfg;
 end
