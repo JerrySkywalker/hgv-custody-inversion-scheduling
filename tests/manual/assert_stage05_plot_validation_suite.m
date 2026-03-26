a08040dbb43293885ae43e6e4b6ccab930bbf79b
JerrySkywalker <jerry1999_main@outlook.com>
2026-03-26 14:15:41 +0800

    test: upgrade Stage05 plot validation suite assert with compare checks

--- tests/manual/assert_stage05_plot_validation_suite.m
+++ tests/manual/assert_stage05_plot_validation_suite.m
@@ -3,6 +3,7 @@ startup;
 
 out = manual_smoke_stage05_plot_validation_suite();
 assert_stage05_plot_validation_suite_result(out);
+assert_stage05_plot_validation_suite_compare_result(out.compare);
 
 disp('assert_stage05_plot_validation_suite passed.');
 end
