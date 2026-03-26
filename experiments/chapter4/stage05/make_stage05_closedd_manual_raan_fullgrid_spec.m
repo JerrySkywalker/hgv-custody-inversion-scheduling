c7d83be288bdfb20b409ea5068b19584323b54e9
JerrySkywalker <jerry1999_main@outlook.com>
2026-03-26 15:26:45 +0800

    feat: add parallel monitor and min_parallel_rows support to Stage05 closedd manual RAAN full-grid spec

--- experiments/chapter4/stage05/make_stage05_closedd_manual_raan_fullgrid_spec.m
+++ experiments/chapter4/stage05/make_stage05_closedd_manual_raan_fullgrid_spec.m
@@ -1,63 +1,123 @@
 function spec = make_stage05_closedd_manual_raan_fullgrid_spec(varargin)
-%MAKE_STAGE05_CLOSEDD_MANUAL_RAAN_FULLGRID_SPEC Build Stage05/ClosedD full-grid manual-RAAN spec.
+%MAKE_STAGE05_CLOSEDD_MANUAL_RAAN_FULLGRID_SPEC Build Stage05 ClosedD manual-RAAN full-grid spec.
 
 p = inputParser;
 addParameter(p, 'profile', [], @(x) isstruct(x) || isempty(x));
-addParameter(p, 'i_grid_deg', [60], @(x) isnumeric(x) && ~isempty(x));
-addParameter(p, 'P_grid', [8 10 12], @(x) isnumeric(x) && ~isempty(x));
-addParameter(p, 'T_grid', [8 10 12], @(x) isnumeric(x) && ~isempty(x));
+addParameter(p, 'i_grid_deg', [30 40 50 60 70 80 90], @(x) isnumeric(x) && ~isempty(x));
+addParameter(p, 'P_grid', [4 6 8 10 12], @(x) isnumeric(x) && ~isempty(x));
+addParameter(p, 'T_grid', [4 6 8 10 12 16], @(x) isnumeric(x) && ~isempty(x));
 addParameter(p, 'h_fixed_km', 1000, @(x) isnumeric(x) && isscalar(x));
-addParameter(p, 'F_fixed', 0, @(x) isnumeric(x) && isscalar(x));
-addParameter(p, 'raan_range_deg', [0 20], @(x) isnumeric(x) && numel(x)==2);
+addParameter(p, 'F_fixed', 1, @(x) isnumeric(x) && isscalar(x));
+addParameter(p, 'raan_range_deg', [0 20], @(x) isnumeric(x) && numel(x) == 2);
 addParameter(p, 'raan_step_deg', 10, @(x) isnumeric(x) && isscalar(x) && x > 0);
 addParameter(p, 'plot_visible', 'off', @(x) ischar(x) || isstring(x));
 addParameter(p, 'artifact_root', fullfile('outputs','experiments','chapter4','stage05_closedd_manual_raan'), @(x) ischar(x) || isstring(x));
-addParameter(p, 'output_suffix', 'fullgrid', @(x) ischar(x) || isstring(x));
+addParameter(p, 'output_suffix', 'formal', @(x) ischar(x) || isstring(x));
 addParameter(p, 'save_cache', false, @(x) islogical(x) || isnumeric(x));
-addParameter(p, 'use_parallel', false, @(x) islogical(x) || isnumeric(x));
+addParameter(p, 'use_parallel', true, @(x) islogical(x) || isnumeric(x));
 addParameter(p, 'show_progress', false, @(x) islogical(x) || isnumeric(x));
+addParameter(p, 'parallel_monitor', struct(), @(x) isstruct(x) || isempty(x));
+addParameter(p, 'min_parallel_rows', [], @(x) isempty(x) || (isnumeric(x) && isscalar(x)));
 parse(p, varargin{:});
 args = p.Results;
 
-if isempty(args.profile)
+profile = args.profile;
+if isempty(profile)
     profile = make_profile_MB_nominal_validation_stage05();
-else
-    profile = args.profile;
 end
 
-rows = local_make_fullgrid_rows(args.h_fixed_km, args.i_grid_deg, args.P_grid, args.T_grid, args.F_fixed);
+cfg_base = default_params();
+cfg_base = stage09_prepare_cfg(cfg_base);
+cfg_base = configure_stage_output_paths(cfg_base);
 
-spec = make_stage05_closedd_manual_raan_spec( ...
-    'profile', profile, ...
-    'base_rows', rows, ...
-    'raan_range_deg', args.raan_range_deg, ...
-    'raan_step_deg', args.raan_step_deg, ...
-    'plot_visible', args.plot_visible, ...
-    'artifact_root', args.artifact_root, ...
-    'output_suffix', args.output_suffix, ...
-    'save_cache', args.save_cache, ...
-    'use_parallel', args.use_parallel, ...
-    'show_progress', args.show_progress);
-end
+cfg_overlay = config_service(profile);
 
-function rows = local_make_fullgrid_rows(h_fixed_km, i_grid_deg, P_grid, T_grid, F_fixed)
-rows_cell = {};
-idx = 0;
-for ii = 1:numel(i_grid_deg)
-    for ip = 1:numel(P_grid)
-        for it = 1:numel(T_grid)
-            idx = idx + 1;
-            row = struct();
-            row.design_id = sprintf('FG_i%02d_P%d_T%d', round(i_grid_deg(ii)), P_grid(ip), T_grid(it));
-            row.h_km = h_fixed_km;
-            row.i_deg = i_grid_deg(ii);
-            row.P = P_grid(ip);
-            row.T = T_grid(it);
-            row.F = F_fixed;
-            row.Ns = row.P * row.T;
-            rows_cell{idx,1} = row;
-        end
-    end
+spec = struct();
+spec.cfg_base = cfg_base;
+spec.cfg_overlay = cfg_overlay;
+spec.evaluator_mode = 'closedd';
+spec.design_grid = manual_make_stage05_fullgrid( ...
+    'i_grid_deg', args.i_grid_deg, ...
+    'P_grid', args.P_grid, ...
+    'T_grid', args.T_grid, ...
+    'h_fixed_km', args.h_fixed_km, ...
+    'F_fixed', args.F_fixed);
+
+spec.task_family_builder = @(cfg, spec_in) task_family_service(cfg);
+
+spec.region_phase = struct();
+spec.region_phase.enable = true;
+spec.region_phase.mode = 'manual';
+spec.region_phase.parameter = 'raan_deg';
+spec.region_phase.range_deg = args.raan_range_deg;
+spec.region_phase.step_deg = args.raan_step_deg;
+spec.region_phase.output_mode = 'expand_rows';
+
+search_spec = struct();
+search_spec.gamma_eff_scalar = profile.gamma_eff_scalar;
+search_spec.run_tag = char(string(args.output_suffix));
+search_spec.save_cache = logical(args.save_cache);
+search_spec.use_parallel = logical(args.use_parallel);
+search_spec.show_progress = logical(args.show_progress);
+search_spec.parallel_monitor = args.parallel_monitor;
+if ~isempty(args.min_parallel_rows)
+    search_spec.min_parallel_rows = args.min_parallel_rows;
 end
-rows = vertcat(rows_cell{:});
+search_spec.logger = struct( ...
+    'enable_console', true, ...
+    'console_level', 'INFO', ...
+    'enable_file', false);
+
+spec.search_spec = search_spec;
+
+req1 = struct();
+req1.type = 'truth_table';
+req1.name = 'truth_table';
+
+req2 = struct();
+req2.type = 'scenario_aggregate';
+req2.name = 'agg_by_base_design';
+req2.group_keys = {'base_design_id','P','T','Ns'};
+req2.metric_names = {'DG_rob','DA_rob','DT_rob','joint_margin','pass_ratio'};
+req2.aggregate_modes = {'min','max','mean'};
+
+req3 = struct();
+req3.type = 'raan_aware_envelope';
+req3.name = 'env_min_joint_margin';
+req3.group_key = 'Ns';
+req3.metric_name = 'joint_margin';
+req3.fixed_filters = struct();
+req3.scenario_metric = 'joint_margin';
+req3.scenario_mode = 'min';
+
+req4 = struct();
+req4.type = 'raan_aware_envelope';
+req4.name = 'env_min_pass_ratio';
+req4.group_key = 'Ns';
+req4.metric_name = 'pass_ratio';
+req4.fixed_filters = struct();
+req4.scenario_metric = 'pass_ratio';
+req4.scenario_mode = 'min';
+
+req5 = struct();
+req5.type = 'raan_aware_heatmap_slice';
+req5.name = 'hm_min_joint_margin';
+req5.metric_name = 'joint_margin';
+req5.fixed_filters = struct();
+req5.row_key = 'P';
+req5.col_key = 'T';
+req5.scenario_metric = 'joint_margin';
+req5.scenario_mode = 'min';
+
+req6 = struct();
+req6.type = 'raan_aware_heatmap_slice';
+req6.name = 'hm_min_pass_ratio';
+req6.metric_name = 'pass_ratio';
+req6.fixed_filters = struct();
+req6.row_key = 'P';
+req6.col_key = 'T';
+req6.scenario_metric = 'pass_ratio';
+req6.scenario_mode = 'min';
+
+spec.output_requests = {req1, req2, req3, req4, req5, req6};
 end
