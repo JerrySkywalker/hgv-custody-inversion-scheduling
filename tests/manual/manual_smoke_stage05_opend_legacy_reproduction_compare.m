6d54c8838c2218b894e9a21ce3c62d5b7c2e6cff
JerrySkywalker <jerry1999_main@outlook.com>
2026-03-26 13:29:42 +0800

    refactor: allow Stage05 legacy reproduction compare to reuse existing framework result

--- tests/manual/manual_smoke_stage05_opend_legacy_reproduction_compare.m
+++ tests/manual/manual_smoke_stage05_opend_legacy_reproduction_compare.m
@@ -1,17 +1,25 @@
-function out = manual_smoke_stage05_opend_legacy_reproduction_compare()
+function out = manual_smoke_stage05_opend_legacy_reproduction_compare(varargin)
 startup;
 
-artifact_root = fullfile('outputs','experiments','chapter4','stage05_legacy_reproduction','compare_smoke');
+p = inputParser;
+addParameter(p, 'framework_result', [], @(x) isempty(x) || isstruct(x));
+addParameter(p, 'artifact_root', fullfile('outputs','experiments','chapter4','stage05_legacy_reproduction','compare_smoke'), @(x) ischar(x) || isstring(x));
+parse(p, varargin{:});
+args = p.Results;
 
-fw = run_stage05_opend_legacy_reproduction_framework( ...
-    'i_grid_deg', [30 40 50 60 70 80 90], ...
-    'P_grid', [4 6 8 10 12], ...
-    'T_grid', [4 6 8 10 12 16], ...
-    'h_fixed_km', 1000, ...
-    'F_fixed', 1, ...
-    'plot_visible', 'off', ...
-    'artifact_root', artifact_root, ...
-    'output_suffix', 'compare_smoke');
+if isempty(args.framework_result)
+    fw = run_stage05_opend_legacy_reproduction_framework( ...
+        'i_grid_deg', [30 40 50 60 70 80 90], ...
+        'P_grid', [4 6 8 10 12], ...
+        'T_grid', [4 6 8 10 12 16], ...
+        'h_fixed_km', 1000, ...
+        'F_fixed', 1, ...
+        'plot_visible', 'off', ...
+        'artifact_root', char(string(args.artifact_root)), ...
+        'output_suffix', 'compare_smoke');
+else
+    fw = args.framework_result;
+end
 
 legacy_best = manual_compare_stage05_best_envelope_fullgrid_all_i();
 legacy_hm_i60 = manual_compare_stage05_heatmap_fullgrid_i60();
@@ -21,9 +29,6 @@ out.framework = fw;
 out.legacy_best = legacy_best;
 out.legacy_hm_i60 = legacy_hm_i60;
 
-% -----------------------------
-% Best-pass compare
-% -----------------------------
 fw_best = fw.outputs.best_pass_by_Ns(:, {'Ns','pass_ratio'});
 
 lg_best_raw = legacy_best.compare_table(:, {'Ns','legacy_best_pass'});
@@ -36,9 +41,6 @@ lg_best.legacy_pass_ratio = splitapply(@max, lg_best_raw.legacy_pass_ratio, Gbes
 best_cmp = outerjoin(fw_best, lg_best, 'Keys', 'Ns', 'MergeKeys', true);
 best_cmp.pass_abs_diff = abs(best_cmp.pass_ratio - best_cmp.legacy_pass_ratio);
 
-% -----------------------------
-% Heatmap compare at i=60
-% -----------------------------
 fw_hm = fw.outputs.geometry_heatmap_i60;
 fw_hm_tbl = local_heatmap_to_table(fw_hm, 'framework_DG_rob');
 
