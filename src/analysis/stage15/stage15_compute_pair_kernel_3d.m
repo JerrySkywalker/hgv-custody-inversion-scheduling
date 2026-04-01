f4b2ffa524306faacd04b9c13da220c35b17de21
JerrySkywalker <jerry1999_main@outlook.com>
2026-04-01 18:51:55 +0800

    stage15f: redefine 3d pair lambda metric as smallest positive eigenvalue

--- src/analysis/stage15/stage15_compute_pair_kernel_3d.m
+++ src/analysis/stage15/stage15_compute_pair_kernel_3d.m
@@ -4,6 +4,12 @@ function kappa2 = stage15_compute_pair_kernel_3d(target_state, sat1_state, sat2_
 %
 % target_state = [x,y,z,vx,vy,vz]
 % sat*_state   = [x,y,z,vx,vy,vz]
+%
+% Important:
+% For 2 sensors in 3D, the full 3x3 Gramian is rank <= 2. Therefore the
+% smallest eigenvalue of the full Gramian is theoretically zero. We thus
+% use the smallest strictly-positive eigenvalue as the pair-plane geometry
+% strength indicator.
 
 assert(numel(target_state) == 6, 'target_state must be 6D.');
 assert(numel(sat1_state) == 6, 'sat1_state must be 6D.');
@@ -26,8 +32,14 @@ cosang = max(-1, min(1, dot(u1, u2)));
 crossing_angle_deg = acosd(cosang);
 
 G = [u1(:), u2(:)] * [u1(:), u2(:)]';
-eigsG = eig(G);
-lambda_min_geom = min(eigsG);
+eigsG = sort(real(eig(G)), 'ascend');
+
+pos_eigs = eigsG(eigsG > 1e-10);
+if isempty(pos_eigs)
+    lambda_min_geom = 0.0;
+else
+    lambda_min_geom = min(pos_eigs);
+end
 
 delta_h1 = target_state(3) - sat1_state(3);
 delta_h2 = target_state(3) - sat2_state(3);
@@ -39,4 +51,5 @@ kappa2.delta_h1_norm = delta_h1 / max(box.height_ref_km, eps);
 kappa2.delta_h2_norm = delta_h2 / max(box.height_ref_km, eps);
 kappa2.crossing_angle_deg = crossing_angle_deg;
 kappa2.lambda_min_geom = lambda_min_geom;
+kappa2.rank_proxy = numel(pos_eigs);
 end
