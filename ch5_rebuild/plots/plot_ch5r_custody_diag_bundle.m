a73102d6e215771cc8569b1810275e1cb101ec9b
JerrySkywalker <jerry1999_main@outlook.com>
2026-04-07 00:12:39 +0800

    fix(ch5r-diag): enable latex interpreter for diagnostic plot labels

--- ch5_rebuild/plots/plot_ch5r_custody_diag_bundle.m
+++ ch5_rebuild/plots/plot_ch5r_custody_diag_bundle.m
@@ -14,6 +14,7 @@ disp(['[diag][' diag_out.phase_name '] plotting: start'])
 t = diag_out.time_s(:);
 files = struct();
 
+% ---------- Figure 1: Vr / MG / FSM ----------
 disp(['[diag][' diag_out.phase_name '] plotting: figure 1 Vr/MG/FSM'])
 fig1 = figure('Visible', visible_mode, 'Color', 'w', 'Position', [100 100 1100 900]);
 
@@ -29,8 +30,8 @@ if ~isempty(diag_out.vr.value)
     end
 end
 grid on;
-ylabel('V_r proxy');
-title([diag_out.phase_name '  Vr / MG / FSM diagnostics']);
+ylabel('$V_r$ proxy', 'Interpreter', 'latex');
+title([diag_out.phase_name '  $V_r$ / $M_G$ / FSM diagnostics'], 'Interpreter', 'latex');
 
 subplot(3,1,2);
 hold on;
@@ -38,7 +39,7 @@ plot(t, diag_out.mg.value, 'LineWidth', 1.2);
 plot(t, diag_out.fsm.eps_warn, '--', 'LineWidth', 1.0);
 plot(t, diag_out.fsm.eps_req,  ':', 'LineWidth', 1.0);
 grid on;
-ylabel('M_G proxy');
+ylabel('$M_G$ proxy', 'Interpreter', 'latex');
 
 subplot(3,1,3);
 stairs(t, diag_out.fsm.state, 'LineWidth', 1.2);
@@ -46,13 +47,14 @@ ylim([-0.25, 2.25]);
 yticks([0 1 2]);
 yticklabels({'SC','DC','LoC'});
 grid on;
-xlabel('time (s)');
-ylabel('custody state');
+xlabel('time (s)', 'Interpreter', 'latex');
+ylabel('custody state', 'Interpreter', 'latex');
 
 files.fig1 = fullfile(output_dir, [lower(diag_out.phase_name) '_diag_vr_mg_fsm.png']);
 saveas(fig1, files.fig1);
 close(fig1);
 
+% ---------- Figure 2: NIS / RMSE / bubble-state ----------
 disp(['[diag][' diag_out.phase_name '] plotting: figure 2 NIS/RMSE/bubble-state'])
 fig2 = figure('Visible', visible_mode, 'Color', 'w', 'Position', [120 120 1100 900]);
 
@@ -62,8 +64,8 @@ if ~isempty(diag_out.nis.value)
     plot(t, diag_out.nis.value, 'LineWidth', 1.2);
 end
 grid on;
-ylabel('NIS proxy');
-title([diag_out.phase_name '  NIS / RMSE / bubble-state']);
+ylabel('NIS proxy', 'Interpreter', 'latex');
+title([diag_out.phase_name '  NIS / RMSE / bubble-state'], 'Interpreter', 'latex');
 
 subplot(3,1,2);
 hold on;
@@ -71,13 +73,13 @@ if any(isfinite(diag_out.rmse))
     plot(t, diag_out.rmse, 'LineWidth', 1.2);
 end
 grid on;
-ylabel('RMSE pos (km)');
+ylabel('RMSE pos (km)', 'Interpreter', 'latex');
 
 subplot(3,1,3);
 hold on;
 yyaxis left;
 stairs(t, double(diag_out.bubble.is_bubble(:)), 'LineWidth', 1.2);
-ylabel('bubble');
+ylabel('bubble', 'Interpreter', 'latex');
 ylim([-0.1 1.1]);
 
 yyaxis right;
@@ -85,9 +87,9 @@ stairs(t, diag_out.fsm.state, 'LineWidth', 1.0);
 ylim([-0.25 2.25]);
 yticks([0 1 2]);
 yticklabels({'SC','DC','LoC'});
-ylabel('custody state');
+ylabel('custody state', 'Interpreter', 'latex');
 grid on;
-xlabel('time (s)');
+xlabel('time (s)', 'Interpreter', 'latex');
 
 files.fig2 = fullfile(output_dir, [lower(diag_out.phase_name) '_diag_nis_rmse_bubble_state.png']);
 saveas(fig2, files.fig2);
